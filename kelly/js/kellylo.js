b91c1520d24e1cb5e3cca2537f7675d593401f97
kelly-lo <lo.kelly626@gmail.com>
2021-04-13 13:17:21 -0700

    Modified Kelly's page to fix sticky navigation bar and text size to follow screen size

--- kelly/js/kellylo.js
+++ kelly/js/kellylo.js
@@ -1,18 +1,33 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".nav-menu");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
 $(document).ready(function() {
     $("#experience-btn").on("click", loadExperience);
     $("#contact-btn").on("click", loadContact);
+    $("#home-btn").on("click", loadHeader);
 });
 
 function loadExperience(e) {
     console.log("Experience button clicked");
     $('html,body').animate({
-        scrollTop: $(".experience-section").offset().top},
+        scrollTop: $(".experience-section").offset().top - $(".nav-menu").height()},
         'slow');
 }
 
 function loadContact(e) {
     console.log("Contact button clicked");
     $('html,body').animate({
-        scrollTop: $(".contact-section").offset().top},
+        scrollTop: $(".contact-section").offset().top - $(".nav-menu").height()},
+        'slow');
+}
+
+function loadHeader(e) {
+    console.log("Home button clicked");
+    $('html,body').animate({
+        scrollTop: $(".header").offset().top - $(".nav-menu").height()},
         'slow');
 }
