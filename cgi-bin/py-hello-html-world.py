8eda806f2b63ed422375c0279fd606188ab04bf2
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-18 21:56:14 -0700

    Python test script

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 from datetime import datetime
 import os
 import html
 
 # print("Cache-Control: no-cache\n");
-print("Content-type: text/html\r\n\r\n");
-print("<html>");
-print("<head>");
-print("<title>Hello, Python!</title>");
-print("</head>");
-print("<body>");
+print("Content-type: text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
 
-print("<h1>Hello, Python!</h1>");
-print("<p>This page was generated with Python! :D</p>");
+print("<h1>Hello, Python!</h1>")
+print("<p>This page was generated with Python! :D</p>")
 
-dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y");
-print("<p>Current Time: {}</p>".format(dt_string));
+dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y")
+print("<p>Current Time: {}</p>".format(dt_string))
 
 # IP Address is an environment variable when using CGI
 # address = html.escape(os.environ['REMOTE_ADDR'])
 # print("<p>Your IP Address: {}</p>".format(address))
 
-print("</body>");
-print("</html>");
+print("</body>")
+print("</html>")
