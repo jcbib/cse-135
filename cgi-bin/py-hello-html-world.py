905b36418f42e33a79c8f61059427a71da3de2c4
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-18 20:12:14 -0700

    Python test script

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -17,7 +17,7 @@ dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y")
 print("<p>Current Time: {}</p>".format(dt_string))
 
 # IP Address is an environment variable when using CGI
-address = os.environ['REMOTE_ADDR'];
+address = os.environ.get('REMOTE_ADDR')
 print("<p>Your IP Address: {}</p>".format(address))
 
 print("</body>")
