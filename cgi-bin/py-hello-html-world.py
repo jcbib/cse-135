982f87afac0cf31b37ec47ee5a96f9c896303c33
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-18 20:06:35 -0700

    Python test script

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from datetime import datetime
+import os
+
+print("Cache-Control: no-cache\n")
+print("Content-type: text/html\n\n")
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
+
+print("<h1>Hello, Python!</h1>")
+print("<p>This page was generated with Python! :D</p>")
+
+dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y")
+print("<p>Current Time: {}</p>".format(dt_string))
+
+# IP Address is an environment variable when using CGI
+address = os.environ['REMOTE_ADDR'];
+print("<p>Your IP Address: {}</p>".format(address))
+
+print("</body>")
+print("</html>")
