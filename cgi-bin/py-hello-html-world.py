347ac54f9290cfa1cf877002698e7c1d9a645e18
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-18 21:28:03 -0700

    Test Python script

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python
-from datetime import datetime
-import os
-# import html
 
-print("Cache-Control: no-cache\n")
-print("Content-type: text/html\r\n\r\n")
-print("<html>")
-print("<head>")
-print("<title>Hello, Python!</title>")
-print("</head>")
-print("<body>")
-
-print("<h1>Hello, Python!</h1>")
-# print("<p>This page was generated with Python! :D</p>")
-
-# dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y")
-# print("<p>Current Time: {}</p>".format(dt_string))
-
-# IP Address is an environment variable when using CGI
-# address = html.escape(os.environ['REMOTE_ADDR'])
-# print("<p>Your IP Address: {}</p>".format(address))
-
-print("</body>")
-print("</html>")
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello World - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello World! This is my first CGI program</h2>'
+print '</body>'
+print '</html>'
