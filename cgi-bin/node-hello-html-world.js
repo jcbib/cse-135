f30c28065b6f2b4fead89b9ee335165daacd2776
kelly-lo <lo.kelly626@gmail.com>
2021-04-19 23:55:36 -0700

    Create nodeJS file hello-html-world

---
+++ cgi-bin/node-hello-html-world.js
@@ -0,0 +1,28 @@
+var os = require('os');
+
+console.log("Cache-Control: no-cache")
+console.log("Content-type: text/html\r\n\r\n")
+
+console.log("<html>")
+console.log("<head>")
+console.log("<title>Hello, NodeJS!</title>")
+console.log("</head>")
+console.log("<body>")
+
+console.log("<h1>Hello, NodeJS!</h1>")
+console.log("<p>This page was generated with NodeJS! :D</p>")
+
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var d = new Date();
+
+var dateString = new Date();
+console.log("<p>Current Time: %s $s %s</p>", weekday[d.getDay()], months[d.getMonth()], d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getFullYear());
+
+// IP Address is an environment variable when using CGI
+// address = os.environ['REMOTE_ADDR']
+var networkInterfaces = os.networkInterfaces();
+console.log("<p>Your IP Address: {}</p>".format(networkInterfaces))
+
+console.log("</body>")
+console.log("</html>")
