dc6030067dcf78cde25a1e32c0cc12843cfa9f5c
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-19 21:45:28 -0700

    Added hello world .json for PHP

--- cgi-bin/php-hello-html-world.php
+++ cgi-bin/php-hello-html-world.php
@@ -3,17 +3,16 @@
   header("Content-type: text/html");
   print "<html>";
   print "<head>";
-  print "<title>Hello, Perl!</title>";
+  print "<title>Hello, PHP!</title>";
   print "</head>";
   print "<body>";
 
-  print "<h1>Jon, Anh, and Kelly were here - Hello, PHP</h1>";
-  print "<p>This page was generated with the Perl programming language</p>";
+  print "<h1>Hello, PHP!</h1>";
+  print "<p>This page was generated with the PHP!!!!! O_O :D</p>";
   
   print "<p>Current Time: " . date("l M d H:i:s Y") . "</p>";
 
   # IP Address is an environment variable when using CGI
-  $address = $ENV{REMOTE_ADDR};
   print "<p>Your IP Address: " . $_SERVER['REMOTE_ADDR'] . "</p>";
 
   print "</body>";
---
+++ cgi-bin/php-hello-json-world.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Cache-Control: no-cache");
+header("Content-type: application/json");
+
+$myObj->title = "Hello, PHP!";
+$myObj->IP = $_SERVER['REMOTE_ADDR'];
+$myObj->time = date("l M d H:i:s Y");
+$myObj->heading = "Hello, PHP!";
+$myObj->message = "This page was generated with the PHP!!!!! O_O :D";
+$address = $ENV{REMOTE_ADDR};
+
+$myJSON = json_encode($myObj);
+
+print $myJSON;
+?>
