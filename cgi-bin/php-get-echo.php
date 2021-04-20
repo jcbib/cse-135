da32f6b4306e946687f089fa3fa7b5b311dd1f8a
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-19 22:43:56 -0700

    Added semicolon

--- cgi-bin/php-get-echo.php
+++ cgi-bin/php-get-echo.php
@@ -14,11 +14,11 @@
 
   print "<b>Raw query string: </b>" . $_SERVER['QUERY_STRING'] . "<br/>";
 
-  print("<table> Formatted Query String:")
+  print("<table> Formatted Query String:");
   
-  // foreach ($_GET as $key=>$val) {
-  //   print "<tr><td>" . $key . ": </td><td>" .  $val . "</td></tr>";
-  // }
+  foreach ($_GET as $key=>$val) {
+    print "<tr><td>" . $key . ": </td><td>" .  $val . "</td></tr>";
+  }
   
   print "</table>";
   print "</body>";
