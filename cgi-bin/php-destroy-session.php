5c0538247a98f9b2f2705a6a75eccc9cc50d30af
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-20 21:34:08 -0700

    Completed PHP Sessions?

---
+++ cgi-bin/php-destroy-session.php
@@ -0,0 +1,20 @@
+<?php
+  
+  header("Cache-Control: no-cache");
+  header("Content-type: text/html");
+
+  session_destroy();
+  
+  print "<html>";
+  print "<head>";
+  print "<title>PHP Session Destroyed</title>";
+  print "</head>";
+  print "<body>";
+  print "<h1>Session Destroyed</h1>";
+  print "<a href=\"/hw2/php-cgiform.html\">Back to the PHP CGI Form</a><br />";
+  print "<a href=\"/cgi-bin/php-sessions-1.php\">Back to Page 1</a><br />";
+  print "<a href=\"/cgi-bin/php-sessions-2.php\">Back to Page 2</a>";
+  print "</body>";
+  print "</html>";
+
+?>
---
+++ cgi-bin/php-sessions-2.php
@@ -0,0 +1,34 @@
+<?php
+  session_start(); // Start the PHP_Session function
+
+  $name = $_SESSION["username"];
+
+  header("Cache-Control: no-cache");
+  header("Content-type: text/html");
+
+  print "<!DOCTYPE html>";
+  print "Content-Type: text/html\n\n";
+  print "<html>";
+  print "<head>";
+  print "<title>PHP Sessions</title>";
+  print "</head>";
+  print "<body>";
+
+  print "<h1>PHP Sessions Page 2</h1>";
+
+  if ($name){
+    print("<p><b>Name:</b> $name");
+  }else{
+    print "<p><b>Name:</b> You do not have a name set</p>";
+  }
+  print "<br/><br/>";
+  print "<a href=\"/cgi-bin/php-sessions-1.php\">Session Page 1</a><br/>";
+  print "<a href=\"/hw2/php-cgiform.html\">PHP CGI Form</a><br />";
+  print "<form style=\"margin-top:30px\" action=\"/cgi-bin/php-destroy-session.php\" method=\"get\">";
+  print "<button type=\"submit\">Destroy Session</button>";
+  print "</form>";
+
+  print "</body>";
+  print "</html>";
+
+?>
