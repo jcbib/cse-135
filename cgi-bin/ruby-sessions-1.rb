92c295a81e2cb54ba5afd2c64409c86cbba22780
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-25 04:52:51 -0700

    Test Ruby

--- cgi-bin/ruby-sessions-1.rb
+++ cgi-bin/ruby-sessions-1.rb
@@ -14,7 +14,7 @@ session = CGI::Session.new(cgi,
   'session_key' => '_rb_sess_id',
   'prefix' => 'pstore_sid_')
 
-session['database_manager'].restore()
+  CGI::Session::PStore.restore()
 
 if cgi.params.has_key?('username') and cgi.params['username'][0] != ''
   session['username'] = cgi.params['username']
