4a9eaa65b5fe387c170a95f7320eaef454d9e825
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-25 14:11:50 -0700

    Ruby Session Cookie Test

--- cgi-bin/ruby-sessions-2.rb
+++ cgi-bin/ruby-sessions-2.rb
@@ -4,15 +4,10 @@ require 'cgi'
 require 'cgi/session'
 require 'cgi/session/pstore'
 
-# data_body = $stdin.read()
-# name = data_body.split('=')[1]
-# tests = ""
 cgi = CGI.new
 
 cookie_string = cgi.cookies['cookie'].to_s()
 cookie = CGI::Cookie.parse(cookie_string)
-# sid = cookie['sess_id']
-
 session = CGI::Session.new(cgi, 'database_manager' => CGI::Session::PStore)
 
 puts "Cache-Control: no-cache"
@@ -32,12 +27,12 @@ else
   puts "<p><b>Name:</b> You do not have a name set</p>"
 end
 
-puts session.session_id
-puts "<br/>"
-# puts sid
-puts cookie_string
-puts cookie.class
-puts cookie
+# puts session.session_id
+# puts "<br/>"
+# # puts sid
+# puts cookie_string
+# puts cookie.class
+# puts cookie
 
 puts "<br/><br/>"
 puts "<a href=\"/cgi-bin/ruby-sessions-1.rb\">Session Page 1</a><br/>"
@@ -50,51 +45,3 @@ puts "</body>"
 puts "</html>"
 
 session.close
-
-
-# #!/usr/bin/perl
-# use CGI;
-# use CGI::Session;
-
-# # print "Cache-Control: no-cache\n";
-
-# # Access Perl Session
-# use CGI::Session;
-
-# # Create a new CGI Object
-# $cgi = CGI->new;
-
-# # Get the Session ID from the Cookie
-# $sid = $cgi->cookie("CGISESSID") || undef;
-# $session = new CGI::Session(undef, $cgi, {Directory=>'/tmp'});
-
-# # Access Stored Data
-# $name = $session->param("username");
-
-# print "Content-Type: text/html\n\n";
-
-# print "<html>";
-# print "<head>";
-# print "<title>Perl Sessions</title>";
-# print "</head>";
-# print "<body>";
-
-# print "<h1>Perl Sessions Page 2</h1>";
-
-# if ($name){
-# 	print("<p><b>Name:</b> $name");
-# }else{
-# 	print "<p><b>Name:</b> You do not have a name set</p>";
-# }
-# print "<br/><br/>";
-# print "<a href=\"/cgi-bin/perl-sessions-1.pl\">Session Page 1</a><br/>";
-# print "<a href=\"/hw2/perl-cgiform.html\">Perl CGI Form</a><br />";
-# print "<form style=\"margin-top:30px\" action=\"/cgi-bin/perl-destroy-session.pl\" method=\"get\">";
-# print "<button type=\"submit\">Destroy Session</button>";
-# print "</form>";
-
-# print "</body>";
-# print "</html>";
-
-
-
