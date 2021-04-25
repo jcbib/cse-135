bceaabfc7e9385826e19e662d4050ef96bb8e044
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-25 01:33:13 -0700

    Test cgi attributes

---
+++ cgi-bin/ruby-env.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require "cgi"
+cgi = CGI.new
+
+puts "Cache-Control: no-cache"
+puts "Content-type: text/html\n\n"
+
+puts "<html>"
+puts "<head>"
+puts "<title>Hello, Ruby!</title>"
+puts "</head>"
+puts "<body>"
+
+puts "<h1 align=center> Environment Variables </h1>"
+puts "<hr/>"
+
+for i in cgi.attributes
+  puts i
+
+puts "</body>"
+puts "</html>"
+
+# print("<html>")
+# print("<head>")
+# print("<title> Environment Variables </title>")
+# print("</head>")
+
+# print("<body>")
+# print("<h1 align=center> Environment Variables </h1>")
+# print("<hr/>")
+
+# for param in os.environ.keys():
+#    print ("<b>{}</b>: {}<br/>".format(param, os.environ[param]))
+
+# print("</body>")
+# print("</html>")
