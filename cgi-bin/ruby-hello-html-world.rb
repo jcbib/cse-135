7a433756513c9d5c0b8bf5ebd7f54033a6567e7f
Jon Carlo Bibat <jonbibat@gmail.com>
2021-04-25 01:24:04 -0700

    Finished Ruby Hello World JSON

--- cgi-bin/ruby-hello-html-world.rb
+++ cgi-bin/ruby-hello-html-world.rb
@@ -24,24 +24,3 @@ puts "<p>Your IP Address: %s</p>" % [address]
 
 puts "</body>"
 puts "</html>"
-
-# print("Cache-Control: no-cache")
-# print("Content-type: text/html\r\n\r\n")
-# print("<html>")
-# print("<head>")
-# print("<title>Hello, Python!</title>")
-# print("</head>")
-# print("<body>")
-
-# print("<h1>Hello, Python!</h1>")
-# print("<p>This page was generated with Python! :D</p>")
-
-# dt_string = datetime.now().strftime("%A %B %d %H:%M:%S %Y")
-# print("<p>Current Time: {}</p>".format(dt_string))
-
-# # IP Address is an environment variable when using CGI
-# address = os.environ['REMOTE_ADDR']
-# print("<p>Your IP Address: {}</p>".format(address))
-
-# print("</body>")
-# print("</html>")
