4ec2017a8961627dfa05735328cc0700023817e7
Anh-Tran-Sys <abtran@ucsd.edu>
2021-05-04 22:42:26 -0700

    collector

--- collector.js
+++ collector.js
@@ -1,17 +1,8 @@
-const url = 'http://collector.jak-cse135.site/';
+const url = 'https://collector.jak-cse135.site/';
 
 let h = new Headers();
 h.append('Accept', 'application/json');
 
-console.log("1");
-
-let req = new Request(url, {
-    method: 'POST',
-    headers: h,
-    mode: 'cors'
-});
-
-console.log("2");
 fetch(url).then(async response => {
     try {
         const data = await response.json()
