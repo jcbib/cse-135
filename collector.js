30cbd869a9026439f7c98d01a49fd57b7e67f039
Anh-Tran-Sys <abtran@ucsd.edu>
2021-05-04 22:20:03 -0700

    collector

--- collector.js
+++ collector.js
@@ -3,12 +3,15 @@ const url = 'https://jak-cse135.site/';
 let h = new Headers();
 h.append('Accept', 'application/json');
 
+console.log("1");
+
 let req = new Request(url, {
     method: 'POST',
     headers: h,
     mode: 'cors'
 });
 
+console.log("2");
 fetch(req)
     .then((response) => {
         if (response.ok) {
