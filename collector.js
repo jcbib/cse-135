d91ce7fd0c4b8cafe2c6ad66e5a214490903c277
Anh-Tran-Sys <abtran@ucsd.edu>
2021-05-04 22:29:41 -0700

    collector

--- collector.js
+++ collector.js
@@ -12,17 +12,6 @@ let req = new Request(url, {
 });
 
 console.log("2");
-fetch(req)
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('BAD HTTP stuff');
-        }
-    })
-    .then((jsonData) => {
-        console.log(jsonData);
-    })
-    .catch((err) => {
-        console.log('ERROR:', err.message);
-    });
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
