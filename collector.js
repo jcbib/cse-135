08be18dd1e965d16a7250ac9f8bb01714ceade20
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-05 21:38:00 -0700

    Testing collector script

--- collector.js
+++ collector.js
@@ -1,17 +1,19 @@
-const url = 'https://collector.jak-cse135.site/';
+// const url = 'https://collector.jak-cse135.site/';
 
-let h = new Headers();
-h.append('Accept', 'application/json');
-h.append("Access-Control-Allow-Origin", "*");
+// let h = new Headers();
+// h.append('Accept', 'application/json');
+// h.append("Access-Control-Allow-Origin", "*");
 
-fetch(url, {
-    mode: 'no-cors'
-}).then(async response => {
-    try {
-        const data = await response.json()
-        console.log('response data?', data)
-    } catch (error) {
-        console.log('Error happened here!')
-        console.error(error)
-    }
-})
+// fetch(url, {
+//     mode: 'no-cors'
+// }).then(async response => {
+//     try {
+//         const data = await response.json()
+//         console.log('response data?', data)
+//     } catch (error) {
+//         console.log('Error happened here!')
+//         console.error(error)
+//     }
+// })
+
+console.log(window.navigator)
