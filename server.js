f9c42b29e525664c94a627aa019aef4deeddda2d
Anh-Tran-Sys <abtran@ucsd.edu>
2021-05-11 23:26:05 -0700

    temp change

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // app.js file
 
 var jsonServer = require('json-server');
@@ -18,32 +17,3 @@ var router = jsonServer.router('db.json');
 server.use(router);
 
 server.listen(3000);
-
-
-// import express from 'express';
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//     return res.send('Received a GET HTTP method');
-// });
-
-// app.post('/', (req, res) => {
-//     return res.send('Received a POST HTTP method');
-// });
-
-// app.put('/', (req, res) => {
-//     return res.send('Received a PUT HTTP method');
-// });
-
-// app.delete('/', (req, res) => {
-//     return res.send('Received a DELETE HTTP method');
-// });
-
-// app.listen(process.env.PORT, () =>
-//     console.log(`Example app listening on port ${process.env.PORT}!`),
-// );
-
-// app.listen(3000, () =>
-//     console.log(`Example app listening on port 3000!`),
-// );
