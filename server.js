030eb89b066b1df7eb56c670762ab2ee59edcf65
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-12 00:03:31 -0700

    Test

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ var jsonServer = require('json-server');
 
 // Returns an Express server
 var server = jsonServer.create();
+var server_2 = jsonServer.create();
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults());
+server_2.use(jsonServer.defaults());
 
 // Add custom routes
-server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) });
-server.get('/custom_2', function(req, res) { res.json({ msg: 'AaSDFAS' })});
+server_2.get('/custom', function (req, res) { res.json({ msg: 'hello' }) });
+server_2.get('/custom_2', function(req, res) { res.json({ msg: 'AaSDFAS' })});
 
 // Returns an Express router
 var router = jsonServer.router('db.json');
+var router_2 = jsonServer.router('db.json');
 
 server.use(router);
+server_2.use(router_2);
 
 server.listen(3000);
+server_2.listen(3001);
