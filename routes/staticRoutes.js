d63870c43404569e7916840c3037ab6d4736777f
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-13 01:27:16 -0700

    Fixed Routes I THINK

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -16,10 +16,11 @@ router.get('/:sessionId', function(req, res) {
  *               screen-width, screen-height, window-width, window-height, connection-type
  */
 router.post('/', function(req, res) { 
-  const db = apiRouter.db;
-  const table = db.get('test');
-  table.push(req.body).write();
-  res.send(req.body);
+  // const db = apiRouter.db;
+  // const table = db.get('test');
+  // table.push(req.body).write();
+  // res.send(req.body);
+  res.json( {msg: 'posted posted' });
 });
 
 router.delete('/:sessionId', function(req, res) {
