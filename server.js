344030d8c426aa4c863a371ade74d31be9983522
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-13 00:41:36 -0700

    Configuring Routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const bodyParser = require('body-parser');
 
-const staticRoutes = require('./routes/staticRoutes.js');
-const performanceRoutes = require('./routes/performanceRoutes.js');
-const activityRoutes = require('./routes/activityRoutes.js');
+// const staticRoutes = require('./routes/staticRoutes.js');
+// const performanceRoutes = require('./routes/performanceRoutes.js');
+// const activityRoutes = require('./routes/activityRoutes.js');
 
 // Returns an Express server
 const json = jsonServer.create();
@@ -12,6 +12,31 @@ const apiServer = jsonServer.create();
 // Returns an Express router
 const jsonRouter = jsonServer.router('data/db.json');
 
+
+const router = server.router('data/db.json');
+
+// Add activity data routes
+router.get('/activity', function (req, res) { 
+  res.json({ msg: 'hello' }); 
+});
+
+router.get('/activity/:sessionId', function(req, res) { 
+  res.json({ msg: 'AaSDFAS' }); 
+});
+
+router.post('/activity', function(req, res) { 
+  console.log(req.body);
+  res.send("Post Success!");
+});
+
+router.delete('/activity/:sessionId', function(req, res) {
+  res.json({ msg: 'you deleted'}); 
+});
+
+router.put('/activity/:sessionId', function(req, res) {
+  res.json({ msg: 'you put something'});
+});
+
 // Set default middlewares (logger, static, cors and no-cache)
 json.use(jsonServer.defaults());
 apiServer.use(jsonServer.defaults());
@@ -19,10 +44,10 @@ apiServer.use(bodyParser.json())
 apiServer.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect Routes
-apiServer.use('/static', staticRoutes);
-apiServer.use('/performance', performanceRoutes);
-apiServer.use('/activity', activityRoutes);
-
+// apiServer.use('/static', staticRoutes);
+// apiServer.use('/performance', performanceRoutes);
+// apiServer.use('/activity', activityRoutes);
+apiServer.use(router);
 json.use(jsonRouter);
 
 json.listen(3000);
