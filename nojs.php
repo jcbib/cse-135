a183935357ed8bceb614eb4bbc4f109ed48526b0
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-16 14:29:49 -0700

    Testing POST through PHP

--- nojs.php
+++ nojs.php
@@ -25,7 +25,7 @@
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $server_output = curl_exec($ch);
