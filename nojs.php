2e1c9291e739509f0bce9334ee1c91541c2ba76b
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-16 14:33:10 -0700

    Reverted Changes Because I Give Up

--- nojs.php
+++ nojs.php
@@ -3,7 +3,7 @@
         // JS is enabled, do not need to do anything.
     } else {
         $cookiesEn = FALSE;
-        $url = 'http://jak-cse135.site/testnojs.php';
+        $url = 'http://jak-cse135.site/api/static';
         setcookie('testcookie', 'hello');
 
         if (isset($_COOKIE['testcookie'])) {
@@ -22,34 +22,22 @@
             'networkConnectionType' => 'JavaScript Disabled, Unable to Retrieve'
         );
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $options = array (
+            'http' => array(
+                'method' => 'POST',
+                'header' => "Content-Type: application/json\r\n". 
+                            "Accept: application/json\r\n",
+                'content' => json_encode($postData)
+            )
+        );
 
-        $server_output = curl_exec($ch);
-        curl_close($ch);
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) {
+            die('Error');
+        }
 
-
-        // $options = array (
-        //     'http' => array(
-        //         'method' => 'POST',
-        //         'header' => "Content-Type: application/json\r\n". 
-        //                     "Accept: application/json\r\n",
-        //         'content' => http_build_query($postData)
-        //     )
-        // );
-
-        // $context = stream_context_create($options);
-        // $result = file_get_contents($url, false, $context);
-        // if ($result === FALSE) {
-        //     die('Error');
-        // }
-        echo "hello";
-        print_r($ch);
-        print_r($server_output);
-        $responseData = json_decode($server_output, TRUE);
+        $responseData = json_decode($result, TRUE);
         print_r($responseData);
     }
 ?>
