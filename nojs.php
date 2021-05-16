d87fca3de101edd7694789bdfe10689e5cf8d8fe
Jon Carlo Bibat <jonbibat@gmail.com>
2021-05-16 14:24:40 -0700

    Testing POST through PHP

--- nojs.php
+++ nojs.php
@@ -47,7 +47,7 @@
         //     die('Error');
         // }
 
-        $responseData = json_decode($result, TRUE);
+        $responseData = json_decode($serever_output, TRUE);
         print_r($responseData);
     }
 ?>
