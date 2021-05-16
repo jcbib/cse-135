420ac5a89471157765737d5c6c3197cc0850e527
kelly-lo <lo.kelly626@gmail.com>
2021-05-15 20:17:45 -0700

    Creating data viz

--- hw3/database.js
+++ hw3/database.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 function createStaticTable() {
   // Call fetch GET
-  const staticUrl = '/api/static/';;
+  const staticUrl = '/api/static/';
   fetch(staticUrl, {
     method: 'GET',
     headers: {
